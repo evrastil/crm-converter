d8252b038b26797b2d1591cba016e6fdd36e156e
Eduard <evrastil@gmail.com>
2019-03-15 16:27:36 +0100

    fix

--- converter.js
+++ converter.js
@@ -2,7 +2,8 @@ const fs = require('fs')
 const xpath = require('xpath.js')
 const dom = require('xmldom').DOMParser
 const csvWriter = require('csv-write-stream')
-const writer = csvWriter()
+const h2p = require('html2plaintext')
+const writer = csvWriter({separator: '$'})
 
 fs.readFile('export.xml', 'utf8', (err, contents) => {
     if (err) {
@@ -30,18 +31,32 @@ fs.readFile('export.xml', 'utf8', (err, contents) => {
         return ln
     })
     const articles = xpath(doc, '//article').map(element => {
+        const shortDescriptionKeys = Object.keys(element.childNodes).filter(key=> element.childNodes[key].tagName ==='shortDescription')
+        const longDescriptionKeys = Object.keys(element.childNodes).filter(key=> element.childNodes[key].tagName ==='longDescription')
+        const manufacturerKeys = Object.keys(element.childNodes).filter(key=> element.childNodes[key].tagName ==='manufacturer')
+        const nettoKeys = Object.keys(element.childNodes).filter(key=> element.childNodes[key].tagName ==='nettoPrice')
+        const bruttoKeys = Object.keys(element.childNodes).filter(key=> element.childNodes[key].tagName ==='bruttoPrice')
+        const imageKeys = Object.keys(element.childNodes).filter(key=> element.childNodes[key].tagName ==='images')
+        const categoryKeys = Object.keys(element.childNodes).filter(key=> element.childNodes[key].tagName ==='categoryId')
+        const shortTitleKeys = Object.keys(element.childNodes).filter(key=> element.childNodes[key].tagName ==='shortTitle')
+
         const na = ''
-        const shortDescription = element.childNodes['2'].firstChild
-        const manufacturer = element.childNodes['5'].firstChild
-        const nettoprice = element.childNodes['6'].firstChild
-        const bruttoprice = element.childNodes['7'].firstChild
-        const images = element.childNodes['12'].firstChild
-        const cat = element.childNodes['14'].firstChild
-        const shortTitle = element.childNodes['0'].firstChild
+        const shortDescription = element.childNodes[shortDescriptionKeys[0]].firstChild
+        const longDescription = element.childNodes[longDescriptionKeys[0]].firstChild
+        const manufacturer = element.childNodes[manufacturerKeys[0]].firstChild
+        const nettoprice = element.childNodes[nettoKeys[0]].firstChild
+        const bruttoprice = element.childNodes[bruttoKeys[0]].firstChild
+        const images = element.childNodes[imageKeys[0]].firstChild
+        const cat = element.childNodes[categoryKeys[0]].firstChild
+        const shortTitle = element.childNodes[shortTitleKeys[0]].firstChild
+
+        const sanitize = (content) => content.replace(/[\r\n\x0B\x0C\u0085\u2028\u2029]+/g,' ')
+
         return {
-            manufacturer: manufacturer ? manufacturer.data : na,
-            shortTitle: shortTitle ? shortTitle.data : na,
-            shortDescription: shortDescription ? shortDescription.data : na,
+            manufacturer: manufacturer ? sanitize(manufacturer.data) : na,
+            shortTitle: shortTitle ? sanitize(shortTitle.data) : na,
+            shortDescription: shortDescription ? sanitize(shortDescription.data) : na,
+            longDescription: longDescription? sanitize(h2p(longDescription.data)): na,
             nettoprice: nettoprice ? nettoprice.data : na,
             bruttoprice: bruttoprice ? bruttoprice.data : na,
             images: images && images.firstChild ? images.firstChild.data : na,
